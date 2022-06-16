3be29a058dd6730fb4950637014d825d7649ae68
fnooub <61742373+fnooub@users.noreply.github.com>
2022-06-16 04:25:26 -0700

    Add files via upload

---
+++ scan_dichngay.php
@@ -0,0 +1,6 @@
+<?php
+
+include 'functions.php';
+
+echo single_curl('http://vietphrase.info/VietPhrase/Browser?url=https://sacx.herokuapp.com/scan.php?link=http://res.cloudinary.com/fivegins/raw/upload/v1655377702/luufiles/%E5%8F%AF%E6%80%9C%E7%9A%84%E5%A6%88%E5%A6%88_%E7%AC%AC%E4%B8%80%E9%83%A8_%E4%B8%80_qqstao.txt&script=false&t=VP');
+
